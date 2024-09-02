098853046f12708ec4a4233d7c676742af6e6d88
AbdilahiMJama <abdilahi.jama@mnsu.edu>
2024-09-02 18:27:54 -0500

    Added a home landing page

--- app/Home/page.js
+++ app/Home/page.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import {Box, Container, Typography} from '@mui/material';
+import {Box, Container, Grid, Typography, ImageListItem, ImageList} from '@mui/material';
 import Head from 'next/head';
 import picnicBasket from '../../public/picnicbasket.jpg';
 import Image from 'next/image';
 //import { ThemeProvider } from '@mui/material/styles'; 
-
+const itemData = [
+    {
+        img: '/milk.png',
+        title: 'Image',
+    },
+    {
+        img: '/apple.png',
+        title: 'Image',
+    }
+]
 export default function Home() {
   return (
   <Container maxWidth="99vw">
@@ -26,6 +35,19 @@ export default function Home() {
       </Box>
       <Typography variant={'h6'} my={1}>Your Detailed Pantry Tracking App</Typography> 
     </Box>
+    <ImageList sx={{width:500, height:450, marginLeft:55}} cols={3} rowHeight={164}>
+        {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+                <img
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=1`}
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                alt={item.title}
+                loading='lazy'
+                />
+            </ImageListItem>
+        ))}
+
+    </ImageList>
   </Container>
   );
 }
